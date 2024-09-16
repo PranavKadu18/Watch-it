7f46e601973d9270e9ca7c269e2759235045fc50
sham <sham@gmail.com>
2024-09-16 23:00:40 +0530

    timeout increased

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
+    timeout: 10000,
     headers: {accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDY0ODFkNGE5NmU2NDY2MDgwOWJhZmUzMjU0ODU4MyIsIm5iZiI6MTcyNTkxNDIzOC4xMTg1MSwic3ViIjoiNjZkZjQ4NTFiNzBjYzdmNmZkNzhhN2U3Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.AH9MhoFGqWHRKMH8Lmemhxulk20ZVmS5jHj5-0F27-k' 
     }
